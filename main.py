58659b67d5fabf6f3a6cb1a71f8995b9d66d5700
Santosh-kiran <Pskksa0901@gmail.com>
2026-02-22 17:39:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,19 +13,23 @@ OUTPUT_DIR = "outputs"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
+@app.get("/")
+def health():
+    return {"status": "running"}
+
 @app.post("/upload")
 async def upload_resume(file: UploadFile = File(...)):
-    file_path = f"{UPLOAD_DIR}/{file.filename}"
-    
+    file_path = os.path.join(UPLOAD_DIR, file.filename)
+
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-    extracted_text = extract_text(file_path)
-    
-    output_file, output_name = generate_docx(extracted_text)
-    
+    text = extract_text(file_path)
+
+    output_path, filename = generate_docx(text)
+
     return FileResponse(
-        path=output_file,
-        filename=output_name,
-        media_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+        path=output_path,
+        filename=filename,
+        media_type="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
     )
